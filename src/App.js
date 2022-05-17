719ceba6ac33449c1ec2d48aa2d594ea327ed6c2
hjseong <hjseong1222@gmail.com>
2022-05-17 14:53:38 +0900

    state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ArrowComponent from "./components/ArrowComponent";
 import MyLogin from "./components/MyLogin";
 import ArrowLogin from "./components/ArrowLogin";
 import StateComponent from "./components/StateComponent";
+import ArrowState from "./components/ArrowState";
 
 // 함수형 컴포넌트
 function App() {
@@ -25,6 +26,7 @@ function App() {
           // props속성의 이름을 통해서 props값을 전달할수 있다
         />
         <StateComponent />
+        <ArrowState />
       </header>
     </div>
   );
